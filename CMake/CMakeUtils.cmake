2a061a6f64ae9e9057b9d4b3f9de93d1a70b77f7
batuhanbozyel <batuhanbozyel@gmail.com>
2025-03-08 23:56:22 +0300

    LLVM is no longer a submodule, is now required to be installed by default

---
+++ CMake/CMakeUtils.cmake
@@ -0,0 +1,28 @@
+#working directory
+macro(SET_WORKING_DIRECTORY TARGET_NAME ROOT_PATH)
+	if(WIN32)
+		set_target_properties(
+    		${TARGET_NAME} PROPERTIES
+			VS_DEBUGGER_WORKING_DIRECTORY ${ROOT_PATH})
+	else()
+		set_target_properties(
+			${TARGET_NAME} PROPERTIES
+			XCODE_SCHEME_WORKING_DIRECTORY ${ROOT_PATH})
+	endif()
+endmacro()
+
+function(DEPLOY_FILE SRC)
+    foreach(OUTPUTCONFIG ${CMAKE_CONFIGURATION_TYPES})
+        set(DEPLOY_FILES_DST ${CMAKE_BINARY_DIR}/${OUTPUTCONFIG})
+        message(STATUS "Copying ${SRC} to ${DEPLOY_FILES_DST}.")
+        file(COPY ${SRC} DESTINATION ${DEPLOY_FILES_DST})
+    endforeach()
+endfunction()
+
+function(DEPLOY_FILE_TO SRC DST)
+    foreach(OUTPUTCONFIG ${CMAKE_CONFIGURATION_TYPES})
+        set(DEPLOY_FILES_DST ${CMAKE_BINARY_DIR}/${OUTPUTCONFIG}/${DST})
+        message(STATUS "Copying ${SRC} to ${DEPLOY_FILES_DST}.")
+        file(COPY ${SRC} DESTINATION ${DEPLOY_FILES_DST})
+    endforeach()
+endfunction()
