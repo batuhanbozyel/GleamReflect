0549ddc096481c0c8918520814ed8bf10cea8666
batuhanbozyel <batuhanbozyel@gmail.com>
2025-05-19 04:02:50 +0300

    Removed project generation scripts

--- Mac-GenerateProjects.command
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd "`dirname "$0"`"
-chmod u+x ./Mac-GenerateProjects.sh
-./Mac-GenerateProjects.sh
--- Win-GenerateProjects.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-IF EXIST build rmdir /s /q build
-IF EXIST bin rmdir /s /q bin
-mkdir build
-cd build
-cmake -G "Visual Studio 17 2022" ..
-PAUSE
